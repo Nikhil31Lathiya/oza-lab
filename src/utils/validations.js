45bd759ec47b650991df02ce38454c66e3f2ef49
Nikhil31Lathiya <nikhil.lathiya3112@gmail.com>
2023-02-02 11:27:46 +0530

    validation errors

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,11 +1,19 @@
 import Joi from 'joi'
+// dob: Joi.date().format('YYYY-MM-DD').options({ convert: false }),
+// role: Joi.array().items(Joi.string().valid('User', 'Admin', 'SuperAdmin').insensitive()),
+// gender: Joi.array().items(Joi.string().valid('male', 'female').insensitive()),
 
 export const createUserSchema = (body) => {
   return Joi.object({
     email: Joi.string().email().required(),
-    name: Joi.string().max(30).min(2).required(),
-    nickname: Joi.string().optional().allow('')
-
+    firstName: Joi.string().max(30).min(2).required(),
+    lastName: Joi.string().optional().allow(''),
+    role: Joi.string().valid('User', 'Admin', 'SuperAdmin'),
+    gender: Joi.string().valid('male', 'female'),
+    title: Joi.string().valid('mr', 'ms', 'baby', 'master', 'dr'),
+    // dob: Joi.string(),
+    address: Joi.string().max(100).min(2).required(),
+    contact: Joi.string().length(10).pattern(/^[0-9]+$/).required()
   }).validate(body)
 }
 
