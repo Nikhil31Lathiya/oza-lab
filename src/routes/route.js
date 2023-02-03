efe3034c5bfa232a37c34e844e1bdf721361393e
Ayush765 <ayush.chauhanbs@gmail.com>
2023-02-03 23:12:17 +0530

    swagger of test 85% done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,9 @@
-import express from 'express'
-import userRoute from './user/user.route.js';
-import testRoute from './tests/test.route.js';
-const route = express.Router()
+import express from "express";
+import userRoute from "./user/user.route.js";
+import testRoute from "./tests/test.route.js";
+const route = express.Router();
 
-route.use('/user', userRoute);
-route.use('/test', testRoute);
+route.use("/user", userRoute);
+route.use("/tests", testRoute);
 
 export default route;
