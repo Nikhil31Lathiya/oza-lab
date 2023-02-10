a1f920dc276b55f7c6c8529d008f6726afe6fb77
Ayush765 <ayush.chauhanbs@gmail.com>
2023-02-10 14:22:40 +0530

    user removed

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,9 @@
 import express from "express";
-import userRoute from "./user/user.route.js";
 import testRoute from "./tests/test.route.js";
 import patientRoute from "./patient/patient.route.js";
 const route = express.Router();
 
-route.use("/users", userRoute);
+
 route.use("/tests", testRoute);
 route.use("/patients", patientRoute);
 
