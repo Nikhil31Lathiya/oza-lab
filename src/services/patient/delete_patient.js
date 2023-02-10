aa52e4710a40f8919537ee2ae63f9374b0a33ac4
Ayush765 <ayush.chauhanbs@gmail.com>
2023-02-10 11:57:48 +0530

    changes done at delete test and patient

--- src/services/patient/delete_patient.js
+++ src/services/patient/delete_patient.js
@@ -2,11 +2,18 @@ import PatientRepository from '../../domain/patient/patient.repo.js'
 
 export async function deletePatient (req, res) {
     const patientRepository = new PatientRepository();
-    const patient = await patientRepository.deletePatient(parseInt(req.params.id));
-    if (patient){
-        return res.status(200).json(patient);
+    const patient = await patientRepository.getPatient(parseInt(req.params.id));
+    if (!patient) {
+        return res.status(404).json({ message: "No patient found" });
         
     }
-    res.status(404).json({message:"Patient not found"})
+    const deletePatient = await patientRepository.deletePatient(parseInt(req.params.id));
+    if (deletePatient) {
+        res.status(204).json({ message: " Patient deleted successfully"});
+    }
+    else{
+        res.status(500).json({ message: "Could not delete a patient" });
+    }
+
 
 }
--- src/services/tests/delete_test.js
+++ src/services/tests/delete_test.js
@@ -4,10 +4,19 @@ import TestRepository from "../../domain/tests/tests.repo.js";
 export async function deleteTest(req, res) {
   const testRepository = new TestRepository();
  
-  const test = await testRepository.deleteTest(parseInt(req.params.id));
-  if (test) {
-    return res.status(200).json(test);
+  const test = await testRepository.getTest(parseInt(req.params.id));
+  if (!test) {
+     return res.status(404).json({ message: "No test found" });
+    
   }
-
-  res.status(404).json({ message: "test not found" });
+  const deleteTest = await testRepository.deleteTest(parseInt(req.params.id));
+  if (deleteTest) {
+        res.status(204).json({ message: " Test deleted successfully" });
+    
+  }
+  else{
+      res.status(500).json({ message: "Could not delete a test" });
+    }
+  
+  
 }
