524b06a9156b503d353afbde0020dd0231bcd381
Nikhil31Lathiya <nikhil.lathiya3112@gmail.com>
2023-02-15 23:34:12 +0530

    Merge branch 'develop' into userReport_nikhil

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import userRoute from './user/user.route.js'
 import userReportRoute from './userReport/userReport.route.js'
+import testRoute from './tests/test.route.js'
+import patientRoute from './patient/patient.route.js'
+
 const route = express.Router()
 
 route.use('/user', userRoute)
 route.use('/userReport', userReportRoute)
+route.use('/tests', testRoute)
+route.use('/patients', patientRoute)
+
 export default route
---
+++ src/services/patient/get_patients.js
@@ -0,0 +1,13 @@
+import PatientRepository from "../../domain/patient/patient.repo.js";
+
+export async function getPatients(req, res) {
+  const patientRepository = new PatientRepository();
+  const patients = await  patientRepository.getPatients();
+  if (patients.length) {
+    return res.status(200).json({message: "patient fetched successfully",patients});
+  }
+
+  res.status(404).json({
+    message: "No Patients found",
+  });
+}
