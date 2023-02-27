5b2c6acae7853241980785abf64ba54d6f9a3398
Nikhil31Lathiya <nikhil.lathiya3112@gmail.com>
2023-02-27 17:33:58 +0530

    creating login-mail-signup

--- src/services/tests/create_test.js
+++ src/services/tests/create_test.js
@@ -1,14 +1,13 @@
-import TestRepository from "../../domain/tests/tests.repo.js";
-import { createTestSchema } from "../../utils/tests_validation.js";
+import TestRepository from '../../domain/tests/tests.repo.js'
+import { createTestSchema } from '../../utils/tests_validation.js'
 
-export async function createTest(req, res) {
-  const testRepository = new TestRepository();
-  const { error, value } = createTestSchema(req.body);
-
+export async function createTest (req, res) {
+  const testRepository = new TestRepository()
+  const { error, value } = createTestSchema(req.body)
 
   if (error) {
-    return res.status(400).json({ [error.name]: error.message });
+    return res.status(400).json({ [error.name]: error.message })
   }
-  const test = await testRepository.createTest(value);
-  res.status(200).json(test);
+  const test = await testRepository.createTest(value)
+  res.status(200).json(test)
 }
